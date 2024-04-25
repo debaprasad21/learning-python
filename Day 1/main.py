24d189f5b91617306929fa09ef1e483dd0875a92
Debaprasad Sahoo <debaprasadworld@gmail.com>
2024-04-25 15:46:32 +0530

    Day 1 Code

---
+++ Day 1/main.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator!")
+city_name = input("Enter the city name where you live: ")
+pet_name = input("Enter your pet name: ")
+print("Your Band Name is " + city_name + " " + pet_name)
